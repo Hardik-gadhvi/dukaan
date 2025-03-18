c1803b139cb1434a3e73844963bcc0be0ae0be96
Hardik-Gadhvi <gadhvi.hardik1159@gmail.com>
2025-03-18 10:54:35 +0530

    Routing has been  added at client side

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -43,6 +43,11 @@ export class ShopService {
       next: response => this.types = response
     })
   }
+
+  getProduct(id: number){
+    return  this.http.get<Product>(this.baseUrl + 'products/' +id);
+  }
+
   getBrands(){
     if(this.brands.length > 0) return;
     return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
